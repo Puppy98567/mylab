dbdfbc3d1b5e64f64b79a46ed80732047449e709
Puppy98567 <153344377+Puppy98567@users.noreply.github.com>
2024-02-05 21:21:09 +0530

    Update and rename sample.txt to StudentInformation.java

---
+++ StudentInformation.java
@@ -0,0 +1,20 @@
+public calss StudentInformation
+  {
+  public static void main(String args[])
+    {
+    printStudentInfo();
+  }
+  private static void printStudentInfo()
+    {
+    String collegeName="AITS";
+      String student="sunny";
+    int rollnumber=258963;
+    System.out.println("colegename:"+collegeName);
+    System.out.println("studentname:"+student);
+    System.out.println("rollno:"+rollnumber);
+       System.out.println("\n welcome,"+student+"!");
+        System.out.println("we hope you have a fantastic journey");
+        System.out.println("kill the sleeping people");
+  }
+}
+ 
